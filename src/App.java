7049735567771cfba4d196d84963135f20ff81c4
Adam Moataz Sami Kamal Ali Hindawi <adam.moataz.hindawi2014@gmail.com>
2025-12-12 19:24:06 +0200

    yo

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sound.sampled.Line;
 
 
 public class App {
@@ -14,4 +15,19 @@ public class App {
         // create a drawing canvas
         final DrawingCanvas canvas = new DrawingCanvas();
     }
+}
+class DrawingCanvas extends JPanel{
+    private Color currentColor = Color.BLACK;
+    private int x1,y1,x2,y2;
+    private List<LineSegment> segments = new ArrayList<>();
+
+public DrawingCanvas(){
+
+}
+
+private static class LineSegmant{
+    int x1,y,x2,y2;
+LineSegmant(){
+
+}
 }
