79ba9febd39af6019471a1687e0b142c3fb0700a
Adam <adam.moataz.hindawi2014@gmail.com>
2025-12-20 13:26:47 +0200

    mr mohammed is the best we fixed line segment

--- src/App.java
+++ src/App.java
@@ -1,12 +1,11 @@
-import javax.swing.*;
+
 import java.awt.*;
-import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.List;
-import javax.sound.sampled.Line;
-
+import javax.swing.*;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         JFrame frame = new JFrame("Simple Paint Program");
         Container container = frame.getContentPane();
@@ -16,18 +15,32 @@ public class App {
         final DrawingCanvas canvas = new DrawingCanvas();
     }
 }
-class DrawingCanvas extends JPanel{
+
+class DrawingCanvas extends JPanel {
+
     private Color currentColor = Color.BLACK;
-    private int x1,y1,x2,y2;
+    private int x1, y1, x2, y2;
     private List<LineSegment> segments = new ArrayList<>();
 
-public DrawingCanvas(){
+    public DrawingCanvas() {
 
-}
+    }
 
-private static class LineSegmant{
-    int x1,y,x2,y2;
-LineSegmant(){
+    public void setCurrentColor(Color color ){
+        currentColor = color;
+    }
 
-}
+    private static class LineSegment {
+
+        int x1, y1, x2, y2;
+        Color color;
+
+        LineSegment(int x1, int y1, int x2, int y2, Color color) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+            this.color = color;
+        }
+    }
 }
