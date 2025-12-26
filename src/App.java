32e28a8944d5fc93566efd57846de5a07926726e
Adam <adam.moataz.hindawi2014@gmail.com>
2025-12-26 19:22:47 +0200

    sleep:D

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.*;
@@ -13,6 +16,19 @@ public class App {
 
         // create a drawing canvas
         final DrawingCanvas canvas = new DrawingCanvas();
+        container.add(canvas, BorderLayout.CENTER);
+
+        // create a color choser
+        JPanel controls = new JPanel();
+        container.add(controls, BorderLayout.NORTH);
+
+        JButton colorButton = new JButton("pick color");
+        controls.add(colorButton);
+        colorButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
     }
 }
 
@@ -23,10 +39,37 @@ class DrawingCanvas extends JPanel {
     private List<LineSegment> segments = new ArrayList<>();
 
     public DrawingCanvas() {
+        setDoubleBuffered(false);
+        setPreferredSize(new Dimension(800, 600));
 
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                x1 = e.getX();
+                y1 = e.getY();
+            }
+
+            public void mouseReleased(MouseEvent e) {
+                x2 = e.getX();
+                y2 = e.getY();
+                segments.add(new LineSegment(x1, y1, x2, y2, currentColor));
+                repaint();
+            }
+
+        });
+
+        addMouseMotionListener(new MouseAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                x2 = e.getX();
+                y2 = e.getY();
+                segments.add(new LineSegment(x1, y1, x2, y2, currentColor));
+                x1 = x2;
+                y1 = y2;
+                repaint();
+            }
+        });
     }
 
-    public void setCurrentColor(Color color ){
+    public void setCurrentColor(Color color) {
         currentColor = color;
     }
 
